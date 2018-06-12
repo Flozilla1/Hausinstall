7c453007f445d8e25494c8683cae78f51db40fee
Unknown <florianschaefer2@gmail.com>
2018-06-12 16:41:51 +0200

    Ziehharmonika fertig soweit
    Ausgewählte Kat wieder schließen fehlt eventuell

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,15 @@
 $(document).ready(function(){
     
-    $(".all_cats").click(function(){
-    
-        $(".all_cats").removeClass("open").addClass("closed");
-        $(this).toggleClass("open").removeClass("closed");
-    });
-})
+    $(".all_cats").click(open);
+//    $(".cat_bar").click(close);
+
+});
+
+function open () {
+    $(".all_cats").removeClass("open").addClass("closed");
+    $(this).addClass("open").removeClass("closed");
+}
+//function close () {
+//    console.log(this.getParentNode)
+//    $(this.getParentNode).removeClass("open").addClass("closed");
+//}
